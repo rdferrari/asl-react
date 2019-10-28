6fbcb73cb47417f87b01c44c42872804bed5f3d0
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-10-28 20:25:35 +1300

    Backgroung 100%

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import LabsList from "../data/labs.json";
+
 // import PeopleList from "../data/people.json";
 
 const KeyPeople = ({ name, position, image }) => {
