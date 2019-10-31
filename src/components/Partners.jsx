43a7487be065f5381ce90c54d5fe987d7a223c4f
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-10-31 20:49:17 +1300

    Europe detail working

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
 class Partners extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = { world: false, oceania: false, nz: false, europe: false };
+  }
+
+  toogleEurope = () => {
+    this.setState({
+      europe: !this.state.europe
+    });
+  };
+
+  _maps() {
     return (
       <div className="partners-container">
         <div className="content-container-black">
@@ -29,6 +40,54 @@ class Partners extends Component {
         </div>
       </div>
     );
+  }
+
+  _world() {
+    const { europe } = this.state;
+
+    return (
+      <div className="partners-container">
+        <div className="content-container-black">
+          <header>
+            <h1>International Partners / Hoa Taiao</h1>
+          </header>
+          <div className="partners-map-detail-container">
+            {europe === true ? (
+              <img
+                onClick={this.toogleEurope}
+                className="partners-europe-detail-image"
+                src="/images/maps/europe-detail.png"
+              />
+            ) : (
+              <div
+                onClick={this.toogleEurope}
+                className="partners-bt-invisible"
+              ></div>
+            )}
+
+            <img
+              className="partners-map-detail-image"
+              src="/images/maps/world-detail.png"
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  _europe() {
+    return (
+      <div className="">
+        <img
+          className="partners-map-detail-image"
+          src="/images/maps/world-detail.png"
+        />
+      </div>
+    );
+  }
+
+  render() {
+    return this._world();
   }
 }
 
