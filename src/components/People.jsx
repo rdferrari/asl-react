425e09313d6c011bacf0ed053eb31a944fe7dd97
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-11-02 17:21:47 +1300

    People links and new people

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -3,30 +3,46 @@ import { Link } from "react-router-dom";
 
 import PeopleList from "../data/people.json";
 
-const KeyPeople = ({ name, position, image }) => {
+const KeyPeople = ({ name, position, image, link }) => {
   return (
     <div className="key-people-container">
       <img className="key-people-image" src={image} />
       <div className="key-people-texts">
-        <p className="key-people-name">{name}</p>
-        <p className="key-people-position">{position}</p>
+        <a href={link} target="_blank">
+          <p className="key-people-name">{name}</p>
+          <p className="key-people-position">{position}</p>
+        </a>
       </div>
     </div>
   );
 };
 
-const Person = ({ name, image }) => {
+const Person = ({ name, image, link }) => {
   return (
     <div className="person-container">
-      <img className="people-image" src={image} />
-      <div className="key-people-texts">
-        <p className="people-name">{name}</p>
-      </div>
+      <a href={link} target="_blank">
+        <img className="people-image" src={image} />
+        <div className="key-people-texts">
+          <p className="people-name">{name}</p>
+        </div>
+      </a>
     </div>
   );
 };
 
 const People = () => {
+  // const PeopleListName = PeopleList.filter(people => people.name > 6);
+
+  const PeopleListName = PeopleList.sort(function(a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
   return (
     <div className="people-container">
       <div className="content-container-black">
@@ -38,11 +54,13 @@ const People = () => {
               name="Barbara Bollard"
               position="Director"
               image="/images/people/barbara.png"
+              link="https://www.aut.ac.nz/profiles?id=em57148&asset=267682"
             />
             <KeyPeople
               name="Pare Keiha"
               position="Chair"
               image="/images/people/pare.png"
+              link="https://www.aut.ac.nz/research/professors-listing/pare-keiha"
             />
           </div>
         </header>
@@ -50,8 +68,13 @@ const People = () => {
         <div className="content-group-people">
           <h2 className="title-black">People (Alphabetical)</h2>
           <div className="people-container">
-            {PeopleList.map(person => (
-              <Person key={person.id} name={person.name} image={person.image} />
+            {PeopleListName.map(person => (
+              <Person
+                key={person.id}
+                name={person.name}
+                image={person.image}
+                link={person.link}
+              />
             ))}
           </div>
         </div>
