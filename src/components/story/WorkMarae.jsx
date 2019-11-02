e9ffaaf14d43a6afeeab13f9ba20c0d7bcf8fc8e
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-11-02 13:18:34 +1300

    Marae work

--- src/components/story/WorkMarae.jsx
+++ src/components/story/WorkMarae.jsx
@@ -7,17 +7,17 @@ const WorkAirBox = () => {
   return (
     <div>
       <Story
-        header="AirBox AR"
-        intro="The AirBox Science project is developed by NIWA in partnership with Massey University, and Auckland University of Technology’s AppLab, with funding from the Unlocking Curious Minds fund provided by the Ministry of Business Innovation and Employment."
-        imageHeader="/images/work-airbox/ar-box-header.jpg"
-        text="The app is a Data visualisation tool that uses AR (augmented reality) to show the experiments about air happening inside the AirBox. It functions by getting sensor data from AirBox unit, then showing you CO2, humidity, and temperature levels in near real time."
-        imageTextOne="/images/work-airbox/ar-box-content.jpg"
+        header="AUT Marae Point Cloud"
+        intro=""
+        imageHeader="/images/work-marae/header.jpg"
       />
 
-      <ImageBottom image="/images/work-airbox/ar-box-detail.jpg" />
-      <ImageBottom image="/images/work-airbox/ar-box-sensor.jpg" />
-      <ImageBottom image="/images/work-airbox/ar-box-close.jpg" />
-      <ImageBottom image="/images/work-airbox/ar-box-header.jpg" />
+      <ImageBottom image="/images/work-marae/bottom1.jpg" />
+      <ImageBottom image="/images/work-marae/bottom2.jpg" />
+      <ImageBottom image="/images/work-marae/bottom3.jpg" />
+      <ImageBottom image="/images/work-marae/bottom4.jpg" />
+      <ImageBottom image="/images/work-marae/bottom5.jpg" />
+      <ImageBottom image="/images/work-marae/bottom6.jpg" />
     </div>
   );
 };
