67d4eb0e94de6bbe19fa0791c56b2e75a3293fa9
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-11-03 15:40:33 +1300

    Our Vision refine scss

--- src/components/OurVision.jsx
+++ src/components/OurVision.jsx
@@ -23,6 +23,7 @@ const OurVision = () => {
           <p className="content-title-eng">
             As man disappears from sight, the land remains.
           </p>
+
           <p className="content-text">
             Whatungarongaro te tangata toitū te whenua. As man disappears from
             sight, the land remains. The New Zealand Art Science Lab at AUT
@@ -32,28 +33,24 @@ const OurVision = () => {
             and scholarly practice. Researchers in the Lab explore ways of
             creating new methods of inquiry in a supportive environment that
             seeks to advance understanding of our living planet through diverse
-            yet complementary knowledge strands. The Lab explores a sense of
-            place and belonging in Aotearoa, creating a platform for a positive
-            environmental future vision. This vision takes into account the
-            importance of our complex ecosystems, acknowledges human and
-            non-human connection and interdependence, and recalibrates notions
-            of value to prioritise kaitiakitanga. When artists and scientists
-            work together unexpected ideas and discoveries emerge.
+            yet complementary knowledge strands.
+          </p>
+          <p className="content-text">
+            The Lab explores a sense of place and belonging in Aotearoa,
+            creating a platform for a positive environmental future vision. This
+            vision takes into account the importance of our complex ecosystems,
+            acknowledges human and non-human connection and interdependence, and
+            recalibrates notions of value to prioritise kaitiakitanga.
+          </p>
+          <p className="content-text">
+            When artists and scientists work together unexpected ideas and
+            discoveries emerge.
           </p>
         </div>
-        <div className="content-container-left">
-          <img
-            className="full-image content-image"
-            src="/images/our-vision-ice.png"
-          />
-          <img
-            className="full-image content-image"
-            src="/images/our-vision-tree.png"
-          />
-          <img
-            className="full-image content-image"
-            src="/images/our-vision-people.png"
-          />
+        <div className="content-container-right">
+          <img className="content-image" src="/images/our-vision-ice.png" />
+          <img className="content-image" src="/images/our-vision-tree.png" />
+          <img className="content-image" src="/images/our-vision-people.png" />
         </div>
       </div>
     </div>
