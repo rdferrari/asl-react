240b97e9889311208cd27bb22a040887f3b0a2b9
Rodrigo Ferrari <rodrigo@MacBook-Pro.local>
2019-11-06 10:10:20 +1300

    Our work responsive

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -5,11 +5,11 @@ import OurWorkList from "../data/our_work.json";
 
 const Work = ({ name, image, link }) => {
   return (
-    <div className="person-container">
+    <div className="">
       <Link to={link}>
-        <img alt="Work example" className="people-image_work" src={image} />
-        <div className="key-people-texts">
-          <p className="people-name">{name}</p>
+        <img alt="Work example" className="our-work-image" src={image} />
+        <div className="">
+          <p className="our-work-name">{name}</p>
         </div>
       </Link>
     </div>
@@ -18,35 +18,32 @@ const Work = ({ name, image, link }) => {
 
 const OurWork = () => {
   return (
-    <div className="people-container">
-      <div className="content-container-black">
+    <div className="main-box">
+      <div className="main-header-box_black">
         <header>
-          <h1 className="tagline">Our Work / Mahi</h1>
-          <div className="header-text-container">
-            <p className="intro-black">
+          <h1 className="main-tagline">Our Work / Mahi</h1>
+          <div className="our-work-text">
+            <p className="our-work-intro">
               The Art Science Lab at AUT expands the horizons of Art and Science
               by means of transdisciplinary research, communicating the
               imperative of preserving Earth’s threatened ecosystems, through
               ethical discourse and scholarly practice.
             </p>
-            <p className="intro-black_sub">
+            <p className="our-work-intro_sub">
               When artists and scientists work together unexpected ideas and
               discoveries emerge. See a selection of our work below.
             </p>
           </div>
         </header>
-
-        <div className="content-group-people">
-          <div className="people-container">
-            {OurWorkList.map(work => (
-              <Work
-                key={work.id}
-                name={work.name}
-                image={work.image}
-                link={work.link}
-              />
-            ))}
-          </div>
+        <div className="our-work-box">
+          {OurWorkList.map(work => (
+            <Work
+              key={work.id}
+              name={work.name}
+              image={work.image}
+              link={work.link}
+            />
+          ))}
         </div>
       </div>
     </div>
